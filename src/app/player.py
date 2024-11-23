d9445292f9182dcb27a9cc973008eb9d934b8e42
Petr Vlasak <petr.vlasakk@gmail.com>
2024-11-23 15:03:25 +0100

    enums, new methods

--- src/app/player.py
+++ src/app/player.py
@@ -1,13 +1,14 @@
 from typing import Dict
 import yfinance as yf
 import pandas as pd
+from events import Career, Property
 
 
 class Player:
     def __init__(self, name: str, age: int, career: str):
         self.name = name
         self.age = age
-        self.career = career
+        self.career = Career
 
         # Financial attributes
         self.cash = 10000.0
@@ -27,12 +28,16 @@ class Player:
         self.health = 100
         self.education = 50
         self.months = 0  # Initialize month counter
+        self.is_married = False
+        self.childern = 0
+        self.property = []
+
 
     # TODO: use real data, make it more realistic... enterpreneur should have more variable income, even negative. Employee should be based on type of job
     def _initialize_income(self) -> float:
-        if self.career == "Student":
+        if self.career == Career.STUDENT:
             return 1500.0
-        elif self.career == "Employee":
+        elif self.career == Career.EMPLOYEE:
             return 4000.0
         else:  # Entrepreneur
             return 3000.0
@@ -88,3 +93,11 @@ class Player:
             # TODO: Use real market data
             monthly_return = 0.007  # About 8.4% annually
             self.investments_value *= 1 + monthly_return
+
+    def buy_a_house(self, property: Property):
+        if self.cash >= 5000000:
+            print('you have enough money to build a house.')
+            self.cash -= 5000000
+            self.property.append(property)
+        else:
+            raise Exception("Not enough money.")
